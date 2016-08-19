4b167316098fba099381d24e8b511453f76b1a77
Thalia <thalia.e.chan@googlemail.com>
2016-08-19 16:31:30 -0700

    Explain cost function assumptions

--- de.differ.js
+++ de.differ.js
@@ -48,6 +48,9 @@ de.differ.prototype.getDiff = function ( tree1, tree2, forestDists ) {
 	}
 };
 
+// Here it is assumed that the costs of inserting, removing and relabelling are equal
+// Hence the cost of any one operation is always 1
+// Modify this for a more complicated cost function, but beware of performance impact
 de.differ.prototype.getNodeDistance = function ( node1, node2 ) {
 	if ( node1 === null && node2 === null ) {
 		return 0;
@@ -77,6 +80,11 @@ de.differ.prototype.getTreeDistances = function ( keyRoot1, keyRoot2, orderedNod
 	for ( i = keyRoot1.leftmost; i < keyRoot1.index + 1; i++ ) {
 		iMinus1 = i === keyRoot1.leftmost ? null : i - 1;
 
+		// Here it is assumed that the costs of inserting, removing and relabelling are equal
+		// Hence, the following calls have been replaced with 1:
+		// this.getNodeDistance( orderedNodes1[i], null )
+		// this.getNodeDistance( null, orderedNodes2[j] )
+		// Modify this for a more complicated cost function, but beware of performance impact
 		for ( j = keyRoot2.leftmost; j < keyRoot2.index + 1; j++ ) {
 			jMinus1 = j === keyRoot2.leftmost ? null : j - 1;
 
