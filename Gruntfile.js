d05230acca423783c7fab01166961af61f85b9fb
Ed Sanders <ejsanders@gmail.com>
2016-10-31 18:31:29 +0000

    Create build task to populate dist folder
    * Concatenate src files into treeDiffer-dist
    * Copy oojs.min to dist folder so we don't have node_modules
      under version control
    * Removed unused 'watch' task
    
    Fixes #4

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 module.exports = function ( grunt ) {
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-concat' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 
@@ -16,24 +17,40 @@ module.exports = function ( grunt ) {
 			},
 			main: [
 				'**/*.js',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!dist/**'
 			]
-		},
-		watch: {
-			files: [
-				'<%= eslint.main %>'
-			],
-			tasks: 'test'
 		},
 		jsonlint: {
 			all: [
 				'**/*.json',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!dist/**'
 			]
+		},
+		concat: {
+			dist: {
+				files: {
+					'dist/treeDiffer-dist.js': [
+						'treeDiffer.js',
+						'treeDiffer.TreeNode.js',
+						'treeDiffer.Tree.js',
+						'treeDiffer.Differ.js'
+					]
+				}
+			}
+		},
+		copy: {
+			dist: {
+				files: {
+					'dist/oojs.min.js': 'node_modules/oojs/dist/oojs.min.js'
+				}
+			}
 		}
 	} );
 
+	grunt.registerTask( 'build', [ 'concat', 'copy' ] );
 	grunt.registerTask( 'lint', [ 'eslint:main', 'jsonlint' ] );
 	grunt.registerTask( 'fix', 'eslint:fix' );
-	grunt.registerTask( 'default', 'lint' );
+	grunt.registerTask( 'default', [ 'lint', 'build' ] );
 };
