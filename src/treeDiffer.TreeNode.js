72de78c7004b243a1205cca9bf8a5d90797f4a51
David Chan <david@troi.org>
2019-08-27 09:37:48 +0100

    Document TreeNode properties

--- src/treeDiffer.TreeNode.js
+++ src/treeDiffer.TreeNode.js
@@ -16,9 +16,24 @@
  * @param {Object} node Object representing a node to be wrapped
  */
 treeDiffer.TreeNode = function ( node ) {
+	/**
+	 * @property {Object} node Object representing the wrapped node
+	 */
 	this.node = node;
+
+	/**
+	 * @property {treeDiffer.TreeNode[]} children Child nodes
+	 */
 	this.children = [];
+
+	/**
+	 * @property {number} index Index in node list ordered by deepest-first then document order
+	 */
 	this.index = null;
+
+	/**
+	 * @property {number} leftmost Leftmost of this node; see treeDiffer.Tree
+	 */
 	this.leftmost = null;
 };
 
@@ -33,13 +48,21 @@ treeDiffer.TreeNode.prototype.addChild = function ( child ) {
 };
 
 /**
+ * @method
  * Check if another TreeNode is equal to this node. Conditions for equality
  * will depend on the use case.
+ *
+ * @param {treeDiffer.TreeNode} other The other TreeNode
+ * @return {boolean} True if equal
  */
 treeDiffer.TreeNode.prototype.isEqual = null;
 
 /**
- * Get the children of the original node wrapped by this tree node. How to
+ * @method
+ *
+ * Get the children of the node that this tree node wraps. How to
  * find and filter children will depend on the use case.
+ *
+ * @return {Object[]} Children of the wrapped node
  */
 treeDiffer.TreeNode.prototype.getOriginalNodeChildren = null;
