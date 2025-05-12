eaf0d31f7520a248382784e1af47e87ab277ca06
Ed Sanders <ejsanders@gmail.com>
2025-05-12 17:20:38 +0100

    Add test suite

--- demo/index.js
+++ demo/index.js
@@ -10,44 +10,7 @@
  * 5. Display the diffs
  */
 
-/* global OO */
-
-// STEP 1
-
-/**
- * Tree node for conducting a tree diff on DOM nodes.
- *
- * @class
- * @extends treeDiffer.TreeNode
- *
- * @constructor
- * @param {Node} node DOM node
- */
-treeDiffer.DomTreeNode = function ( node ) {
-	// Parent constructor
-	treeDiffer.DomTreeNode.parent.call( this, node );
-};
-
-OO.inheritClass( treeDiffer.DomTreeNode, treeDiffer.TreeNode );
-
-// STEP 2
-
-/**
- * Determine whether two tree nodes are equal. Here nodes are considered
- * equal if they have the same tagName (if they are not text nodes), or
- * have the same text content (if they are text nodes).
- *
- * @param {treeDiffer.TreeNode} otherNode The node to compare to this node
- * @return {boolean} Nodes are equal
- */
-treeDiffer.DomTreeNode.prototype.isEqual = function ( otherNode ) {
-	if ( this.node.nodeType === Node.TEXT_NODE ) {
-		return otherNode.node.nodeType === Node.TEXT_NODE &&
-			otherNode.node.textContent === this.node.textContent;
-	} else {
-		return otherNode.node.tagName === this.node.tagName;
-	}
-};
+// STEP 1 & 2 - See DomTreeNode.js
 
 /**
  * Gets children of the original node.
